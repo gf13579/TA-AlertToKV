46732c6641cc1ddac92b1950893bb376796da101
Greg Ford <greg.ford@intalock.com.au>
2022-04-04 01:11:57 +0000

    Switched to loguru

--- bin/create_alert_in_kv.py
+++ bin/create_alert_in_kv.py
@@ -9,7 +9,13 @@ import gzip
 import csv
 import re
 import time
-import logging
+from loguru import logger
+
+# Log to file and stderr (search.log), with INFO the default level
+log_file = os.environ["SPLUNK_HOME"] + "/var/log/splunk/ta-alerttokv.log"
+logger.remove()
+logger.add(sink=log_file, level="INFO")
+logger.add(sink=sys.stderr, level="ERROR")
 
 # For VS Code debugging
 sys.path.append(
@@ -18,16 +24,7 @@ sys.path.append(
 
 # Uncomment to enable debugging with VS Code
 # import splunk_debug as dbg
-# dbg.enable_debugging(timeout=10)
-
-logging.basicConfig(
-    format="%(asctime)s, Level=%(levelname)s, Pid=%(process)s, Logger=%(name)s, File=%(filename)s, Line=%(lineno)s, %(message)s",
-    filename=os.path.join(
-        os.environ["SPLUNK_HOME"], "var", "log", "splunk", "ta-alerttokv.log"
-    ),
-)
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+# dbg.enable_debugging(timeout=5)
 
 now_as_epoch = epoch_time = int(time.time())
 
@@ -42,15 +39,27 @@ config = payload.get("configuration", dict())
 storage_format = config.get("storage_format")
 
 collection = urllib.parse.quote(config.get("collection"))
+if collection == "":
+    msg = "No KV collection name provided as parameter to alert action"
+    logger.log("ERROR", msg)
+    print(msg, file=sys.stderr)
+    sys.exit(3)
+
 app = urllib.parse.quote(config.get("app") if "app" in config else payload.get("app"))
 owner = urllib.parse.quote(config.get("owner") if "owner" in config else "nobody")
 owner = "nobody" if owner == "" else owner
 
+debug = config.get("debug")
+if debug.lower() in ("1", "true"):
+    logger.remove()
+    logger.add(sink=log_file, level="DEBUG")
+    logger.add(sink=sys.stderr, level="DEBUG")
+
+
 # Build the URL for the Splunkd REST endpoint
 kv_url = f"{server_uri}/servicesNS/{owner}/{app}/storage/collections/data/{collection}/batch_save?output_mode=json"
 msg = "Built kvstore url=%s" % kv_url
-print(msg, file=sys.stderr)
-logger.log(level=logging.DEBUG, msg=msg)
+logger.log("DEBUG", msg)
 
 # Process the events, removing internal fields and rendering mv as a json list
 dicts_to_add = []
@@ -67,7 +76,7 @@ with gzip.open(results_file, "rt") as zip_file:
         dicts_to_add.append(new_dict)
 
 msg = f"dicts_to_add is {str(dicts_to_add)}"
-logger.log(level=logging.DEBUG, msg=msg)
+logger.log("DEBUG", msg)
 
 # If we're storing all fields in one kv field - as a json strong
 if storage_format == "json":
@@ -80,9 +89,11 @@ if storage_format == "json":
 alert_info = {k: config[k] for k in config.keys() - {"collection", "app", "owner"}}
 for d in dicts_to_add:
     msg = "Updating d. Config is {}".format(str(config))
-    logger.log(level=logging.DEBUG, msg=msg)
+    logger.log("DEBUG", msg)
     d.update(alert_info)
-    d["search_name"] = search_name
+    # search_name should come from the Splunk saved search, but allow an override for manual `| sendalert` invocation
+    if search_name != "":
+        d["search_name"] = search_name
     d["_time"] = now_as_epoch
 
 headers = {
@@ -95,10 +106,10 @@ try:
         url=kv_url, json=dicts_to_add, headers=headers, verify=False
     )
     msg = "Posted data to KV. Response: {}".format(str(response.json()))
-    logger.log(level=logging.INFO, msg=msg)
+    logger.log("INFO", msg)
 except Exception as e:
     msg = "Server response: {}".format(str(response.json()))
-    logger.log(level=logging.ERROR, msg=msg)
+    logger.log("ERROR", msg=msg)
     msg = "Failed to add records: {}".format(json.dumps(json.loads(e.read())))
     print(msg, file=sys.stderr)
     sys.exit(3)
